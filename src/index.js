713fffb0448f08eae986504bcaa7b84e96d2d892
armeeeeen <armgamer4@gmail.com>
2023-06-06 01:14:45 +0400

    new Project

--- src/index.js
+++ src/index.js
@@ -4,18 +4,8 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-function Image() {
-  return (
-    <div>
-     <img src='https://c2.staticflickr.com/6/5307/5616330443_645de45fc6_b.jpg'/>
-    </div>
-  );
-}
 
-function Hello() {
-  return React.createElement('div',{},'Hello')
-}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-root.render(<Image/>) 
+root.render(<App/>) 
